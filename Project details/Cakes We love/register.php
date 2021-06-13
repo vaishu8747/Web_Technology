661151b16c346c9c8df6c2b60bb59dfcc91ab251
vaishu8747 <64781305+vaishu8747@users.noreply.github.com>
2021-06-13 11:06:07 +0530

    php connection

--- Project details/Cakes We love/register.php
+++ Project details/Cakes We love/register.php
@@ -1,5 +1,36 @@
 <?php
-$servername="localhost";
+$server_name="localhost";
 $username="root";
 $password="";
 $database_name="web_tech";
+
+$conn=mysqli_connect($server_name,$username,$password,$database_name);
+//now check the connection 
+if(!$conn)
+{
+    die("Connection Failed:" . mysqli_connect_error());
+}
+
+if(isset($_POST['save']))
+{
+$name=$_POST['Username'];
+$phone=$_POST['ph'];
+$address=$_POST['Address'];
+$email=$_POST['Email'];
+$password=$_POST['Password'];
+
+$sql_query = "INSERT INTO personal_info (Username,ph,Address,Email,Password)
+VALUES ('$name','$phone','$address','$email','$password')";
+
+if(mysqli_query($conn, $sql_query))
+{
+    echo "New Details Entery inserted Successfully !";
+}
+else
+{
+    echo "Error: " . $sql . "" . mysqli_error($conn);
+}
+mysqli_close($conn);
+
+}
+?>
